0f077bb5bd5004158d269110fe5136a5c92c4611
Ravencod <caillauxr@gmail.com>
2018-10-30 21:15:11 +0100

    reee

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/', to: 'users#new'
+  get '/users', to: 'users#new'
+  post '/', to: 'users#create'
+  post '/users', to: 'users#create'
   get '/users/new', to: 'users#new'
   post '/users/new', to: 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
